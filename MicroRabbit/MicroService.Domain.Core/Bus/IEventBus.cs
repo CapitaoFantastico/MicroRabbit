34b7632df234fb8f9f9a3cf4916a03dd397ff585
Henrique Souza <henriquesouza182@gmail.com>
2021-06-23 17:40:45 -0300

    Implemented interfaces and classes for domain.core

---
+++ MicroRabbit/MicroService.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using MicroService.Domain.Core.Commands;
+using MicroService.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroService.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
---
+++ MicroRabbit/MicroService.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MicroService.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroService.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
