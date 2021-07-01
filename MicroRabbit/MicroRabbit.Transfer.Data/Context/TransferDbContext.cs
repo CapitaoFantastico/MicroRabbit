4a3facafdbd642fbe5c9a57fc89c3e47e7c1a706
Henrique Souza <henriquesouza182@gmail.com>
2021-07-01 01:47:20 -0300

    Initial commit of Transfer Database

--- MicroRabbit/MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
+++ MicroRabbit/MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroRabbit.Transfer.Data.Context
 {
     public class TransferDbContext : DbContext
     {
         public TransferDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
     }
 }
